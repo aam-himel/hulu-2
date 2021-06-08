ab36f515bc8bbee0245e592e737f3cd2b48a17ee
aamhimel <aam.himel@gmail.com>
2021-06-08 07:29:59 +0600

    navbar completed with dynamic query params

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,24 @@
-const NavBar = () => {
+import { useRouter } from "next/router";
+import searchItem from "../utils/searchItem";
+
+const NavBar = (props) => {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1>navbar</h1>
-    </div>
+    <nav className="relative">
+      <div className="flex px-10 sm:px-20  whitespace-nowrap overflow-x-scroll text-2xl space-x-10 sm:space-x-20 scrollbar-hide">
+        {Object.entries(searchItem).map(([key, { title, url }]) => (
+          <h2
+            key={key}
+            className="cursor-pointer transition  duration-100 transform hover:scale-125  active:text-red-400 last:pr-24"
+            onClick={() => router.push(`/?genre=${key}`)}
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
+    </nav>
   );
 };
-
 export default NavBar;
